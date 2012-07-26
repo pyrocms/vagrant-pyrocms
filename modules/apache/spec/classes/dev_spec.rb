23a1d074f38c3eb512b6974e9d833952ba3faef7
Phil Sturgeon <email@philsturgeon.co.uk>
2012-07-26 11:40:51 +0100

    Removed puppet stuff.

--- modules/apache/spec/classes/dev_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'apache::dev', :type => :class do
-
-  it { should include_class("apache::params") }
-  it { should contain_package("apache_dev_package") }
-
-end
--- modules/apache/spec/classes/php_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'apache::php', :type => :class do
-
-  it { should include_class("apache::params") }
-  it { should contain_package("apache_php_package") }
-
-end
